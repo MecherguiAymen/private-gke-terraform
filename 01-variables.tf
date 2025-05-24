a64d13a677145e20f673fea27c265b66a21da55a
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 <mecherguiaymen26@gmail.com>
2025-05-24 22:23:20 +0100

    first commit

---
+++ 01-variables.tf
@@ -0,0 +1,20 @@
+# GCP Project
+variable "gcp_project" {
+  description = "Project in which GCP Resources to be created"
+  type = string
+  default = "sbx-31371-bwbugg8hgxy7gsijq0t9"
+}
+
+# GCP Region
+variable "gcp_region" {
+  description = "Region in which GCP Resources to be created"
+  type = string
+  default = "europe-central2-a"
+}
+
+# artifacr registry and cloud run region
+variable "gcp_region1" {
+  description = "Region in which GCP resource will be created"
+  type = string
+  default = "europe-central2"
+}
